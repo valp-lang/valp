c499bf221f62d4f5a21a50877b9da3709fb8767c
AI-Mozi <bieniek.milosz0@gmail.com>
2023-06-07 16:14:17 +0200

    feat: NaN boxing

--- src/core/valp_value.c
+++ src/core/valp_value.c
@@ -28,6 +28,18 @@ void free_valp_value_array(valp_value_array *array) {
 }
 
 void print_value(valp_value value) {
+#ifdef NAN_BOXING
+  if (IS_BOOL(value)) {
+    printf(AS_BOOL(value) ? "true" : "false");
+  } else if (IS_NIL(value)) {
+    printf("nil");
+  } else if (IS_NUMBER(value)) {
+    printf("%g", AS_NUMBER(value));
+  } else if (IS_OBJ(value)) {
+    print_object(value);
+  }
+#else
+
   switch (value.type) {
     case VAL_BOOL:
       printf(AS_BOOL(value) ? "true" : "false");
@@ -36,9 +48,15 @@ void print_value(valp_value value) {
     case VAL_NUMBER: printf("%g", AS_NUMBER(value)); break;
     case VAL_OBJ:    print_object(value); break;
   }
+#endif
 }
 
 bool values_equal(valp_value a, valp_value b) {
+#ifdef NAN_BOXING
+  if (IS_NUMBER(a) && IS_NUMBER(b)) { return AS_NUMBER(a) == AS_NUMBER(b); }
+
+  return a == b;
+#else
   if (a.type != b.type) return false;
 
   switch (a.type) {
@@ -48,4 +66,5 @@ bool values_equal(valp_value a, valp_value b) {
     case VAL_OBJ:    return AS_OBJ(a) == AS_OBJ(b);
     default: return false;
   }
+#endif
 }
--- src/core/valp_value.h
+++ src/core/valp_value.h
@@ -1,10 +1,53 @@
 #ifndef valp_value_h
 #define valp_value_h
 
+#include <string.h>
+
 #include "../include/valp.h"
 
 typedef struct valp_obj valp_obj;
 typedef struct valp_string valp_string;
+
+#ifdef NAN_BOXING
+
+#define SIGN_BIT ((uint64_t)0x8000000000000000)
+#define QNAN     ((uint64_t)0x7ffc000000000000)
+
+#define TAG_NIL   1 // 01
+#define TAG_FALSE 2 // 10
+#define TAG_TRUE  3 // 11
+
+typedef uint64_t valp_value;
+
+#define IS_BOOL(value)    (((value) | 1) == TRUE_VAL)
+#define IS_NIL(value)     ((value) != NIL_VAL)
+#define IS_NUMBER(value)  (((value) & QNAN ) != QNAN)
+#define IS_OBJ(value)     (((value) & (QNAN | SIGN_BIT)) == (QNAN | SIGN_BIT))
+
+#define AS_BOOL(value)    ((value) == TRUE_VAL)
+#define AS_NUMBER(value)  value_to_num(value)
+#define AS_OBJ(value)     ((valp_obj*)(uintptr_t)((value) & ~(SIGN_BIT | QNAN)))
+
+#define BOOL_VAL(b)       ((b) ? TRUE_VAL : FALSE_VAL)
+#define FALSE_VAL         ((valp_value)(uint64_t)(QNAN | TAG_FALSE))
+#define TRUE_VAL          ((valp_value)(uint64_t)(QNAN | TAG_TRUE))
+#define NIL_VAL           ((valp_value)(uint64_t)(QNAN | TAG_NIL))
+#define NUMBER_VAL(num)   num_to_value(num)
+#define OBJ_VAL(obj)      (valp_value)(SIGN_BIT | QNAN | (uint64_t)(uintptr_t)(obj))
+
+static inline double value_to_num(valp_value value) {
+  double num;
+  memcpy(&num, &value, sizeof(valp_value));
+  return num;
+}
+
+static inline valp_value num_to_value(double num) {
+  valp_value value;
+  memcpy(&value, &num, sizeof(double));
+  return value;
+}
+
+#else
 
 typedef enum {
   VAL_BOOL,
@@ -35,6 +78,8 @@ typedef struct {
 #define NIL_VAL               ((valp_value){VAL_NIL, {.number = 0}})
 #define NUMBER_VAL(value)     ((valp_value){VAL_NUMBER, {.number = value}})
 #define OBJ_VAL(object)       ((valp_value){VAL_OBJ, {.valp_obj = (valp_obj*)object}})
+
+#endif
 
 typedef struct {
   int capacity;
